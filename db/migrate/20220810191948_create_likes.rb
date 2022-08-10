8d2e8ba8c637f5d6f5afc8fd547e94da06766601
unknown <ewoskyx@gmail.com>
2022-08-10 22:29:49 +0300

    Initial commit

---
+++ db/migrate/20220810191948_create_likes.rb
@@ -0,0 +1,9 @@
+class CreateLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.references :author, null: false, foreign_key: {to_table:'authors'}
+      t.references :posts, null: false, foreign_key: {to_table:'posts'}
+      t.timestamps
+    end
+  end
+end
