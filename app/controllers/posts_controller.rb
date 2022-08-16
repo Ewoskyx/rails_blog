4071ee996e03e7fe38dbc7c5c130c2b4327b9003
unknown <ewoskyx@gmail.com>
2022-08-16 16:12:20 +0300

    Added views and dynamic content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.all
+    @posts = @user.posts.includes(:comments, :likes)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments, :likes).find(params[:id])
   end
 end
