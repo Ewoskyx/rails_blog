3a8ac2e54a2f37d955433dfed8ac26a5e0eadea8
unknown <ewoskyx@gmail.com>
2022-08-16 22:48:17 +0300

    Add new post, new comment forms/functions and likes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to user_post_path(id: @comment.post.id),
+                  notice: 'Comment was successfully created!'
+    else
+      render user_post_path(id: @comment.post.id),
+             alert: 'An error has occurred while creating the comment.'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.permit(:text, :post_id)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,13 +8,16 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  # A method that updates the posts counter for a user.
-  def update_posts_counter
-    author.increment!(:posts_counter)
-  end
-
   # A method which returns the 5 most recent comments for a given post.
   def recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+  after_save :update_posts_counter
+  # A method that updates the posts counter for a user.
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
